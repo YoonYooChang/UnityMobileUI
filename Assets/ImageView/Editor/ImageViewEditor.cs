cffbb403a8ce470d8201707ce4d3ec63519bb021
YoonYooChang <jell0202y@gmail.com>
2022-06-13 15:36:13 +0900

    feat: Preview button on Inspector Window.

--- Assets/ImageView/Editor/ImageViewEditor.cs
+++ Assets/ImageView/Editor/ImageViewEditor.cs
@@ -12,5 +12,11 @@ public class ImageViewEditor : Editor
         imageView.BackgroundColor = EditorGUILayout.ColorField("Background Color", imageView.BackgroundColor);
         imageView.Texture = (Texture2D) EditorGUILayout.ObjectField("Texture", imageView.Texture, typeof(Texture2D), true);
         imageView.Download = EditorGUILayout.TextField("Download Link", imageView.Download);
+
+        if (GUILayout.Button("Preview"))
+        {
+            imageView.ShowImageView();
+            Selection.activeGameObject = imageView.ImageObject;
+        }
     }
 }
