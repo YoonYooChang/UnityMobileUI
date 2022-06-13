1abe0e482123590b2d1a5d0bdb00e1867ecfbf40
YoonYooChang <jell0202y@gmail.com>
2022-06-13 15:34:21 +0900

    feat: Variables of ImageView and ImageViewEditor
    Use ImageViewEditor editor script to get ImageView's variables.

--- Assets/ImageView/Editor/ImageViewEditor.cs
+++ Assets/ImageView/Editor/ImageViewEditor.cs
@@ -1,10 +1,16 @@
 using UnityEditor;
+using UnityEngine;
 
 [CustomEditor(typeof(ImageView))]
 public class ImageViewEditor : Editor
 {
     public override void OnInspectorGUI()
     {
-        
+        ImageView imageView = (ImageView)target;
+
+        imageView.ScaleType = (ScaleType) EditorGUILayout.EnumPopup("ScaleType", imageView.ScaleType);
+        imageView.BackgroundColor = EditorGUILayout.ColorField("Background Color", imageView.BackgroundColor);
+        imageView.Texture = (Texture2D) EditorGUILayout.ObjectField("Texture", imageView.Texture, typeof(Texture2D), true);
+        imageView.Download = EditorGUILayout.TextField("Download Link", imageView.Download);
     }
 }
--- Assets/ImageView/Scripts/ImageView.cs
+++ Assets/ImageView/Scripts/ImageView.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Image), typeof(Mask))]
 public class ImageView : MonoBehaviour
 {
-    private void Start()
-    {
-        
-    }
+    public Texture2D Texture;
+    public Color BackgroundColor = Color.black;
+    public ScaleType ScaleType = ScaleType.centerCrop;
+    public string Download;
+
+    [Header("Background")]
+    private Image backgroundImage;
+
+    [Header("Child")]
+    public GameObject ImageObject;
+    private AspectRatioFitter aspectRatio;
+    private RawImage rawImage;
+
 }
