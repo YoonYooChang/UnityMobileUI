9671a6375c15326ee1a34a13a64c827d4b673570
YoonYooChang <jell0202y@gmail.com>
2022-06-13 15:35:47 +0900

    feat: Show texture to ImageView.

--- Assets/ImageView/Scripts/ImageView.cs
+++ Assets/ImageView/Scripts/ImageView.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Image), typeof(Mask))]
@@ -17,4 +20,66 @@ public class ImageView : MonoBehaviour
     private AspectRatioFitter aspectRatio;
     private RawImage rawImage;
 
+    private void Awake() => ShowImageView();
+
+    public void ShowImageView()
+    {
+        CreateImageObject();
+        SetImageView();
+    }
+
+    private void CreateImageObject()
+    {
+        backgroundImage = GetComponent<Image>();
+
+        if (ImageObject != null)
+        {
+            DestroyImmediate(ImageObject);
+        }
+
+        ImageObject = new GameObject("Image");
+        ImageObject.transform.parent = transform;
+        aspectRatio = ImageObject.AddComponent<AspectRatioFitter>();
+        rawImage = ImageObject.AddComponent<RawImage>();
+    }
+
+    private void SetImageView()
+    {
+        if (Texture != null)
+        {
+            SetTexture();
+        }
+        backgroundImage.color = BackgroundColor;
+
+        if (!string.IsNullOrEmpty(Download))
+        {
+            StartCoroutine(GetTexture(Download, texture =>
+            {
+                Texture = texture;
+                SetTexture();
+            }));
+        }
+    }
+
+    private void SetTexture()
+    {
+        rawImage.texture = Texture;
+        rawImage.color = Color.white;
+    }
+
+    private IEnumerator GetTexture(string path, Action<Texture2D> action)
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(path);
+        yield return www.SendWebRequest();
+
+        if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Texture2D texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            action?.Invoke(texture);
+        }
+    }
 }
