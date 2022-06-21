609c14f12d72cb2d0eefd26f6ef360d0ca51024b
YoonYooChang <jell0202y@gmail.com>
2022-06-21 21:38:47 +0900

    feat: SetDynamicGridLayout

--- Assets/GridLayoutGroupHelper/Scripts/GridLayoutGroupHelper.cs
+++ Assets/GridLayoutGroupHelper/Scripts/GridLayoutGroupHelper.cs
@@ -30,6 +30,43 @@ public class GridLayoutGroupHelper : MonoBehaviour
 
         layoutGroupSize = new Vector2(rectTransform.rect.width, rectTransform.rect.height);
 
-        // Calculate
+        SetDynamicGridLayout();
+    }
+
+    private void SetDynamicGridLayout()
+    {
+        int itemCount = this.transform.childCount;
+
+        if (fixColumn)
+        {
+            rowCount = Mathf.CeilToInt(itemCount / colCount);
+        }
+        else
+        {
+            colCount = Mathf.CeilToInt(itemCount / rowCount);
+        }
+
+        float spaceWidth = (gridLayoutGroup.padding.left + gridLayoutGroup.padding.right) + (gridLayoutGroup.spacing.x * (colCount - 1));
+        float spaceHeight = (gridLayoutGroup.padding.top + gridLayoutGroup.padding.bottom) + (gridLayoutGroup.spacing.y * (rowCount - 1));
+
+        Vector2 spaceSize = new Vector2(spaceWidth, spaceHeight);
+        Vector2 displaySize = layoutGroupSize - spaceSize;
+
+        Vector2 displayItemSize;
+        float itemWidth, itemHeight;
+
+        if (fixColumn)
+        {
+            itemWidth = displaySize.x / colCount;
+            itemHeight = (itemWidth / itemSize.x) * itemSize.y;
+        }
+        else
+        {
+            itemHeight = displaySize.y / rowCount;
+            itemWidth = (itemHeight / itemSize.y) * itemSize.x;
+        }
+        displayItemSize = new Vector2(itemWidth, itemHeight);
+
+        gridLayoutGroup.cellSize = displayItemSize;
     }
 }
