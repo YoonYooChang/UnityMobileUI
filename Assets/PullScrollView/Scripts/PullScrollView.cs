a7742b04fc4997fefa6b10bdf3543484dd65478e
YoonYooChang <jell0202y@gmail.com>
2022-07-05 20:30:41 +0900

    PullScrollView Event 추가

--- Assets/PullScrollView/Scripts/PullScrollView.cs
+++ Assets/PullScrollView/Scripts/PullScrollView.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class PullScrollView : ScrollRect, IDropHandler
 {
+    #region Event
+    public UnityEvent OnTopPull;
+    public UnityEvent OnBottomPull;
+    public UnityEvent<float> OnTopPulling;
+    public UnityEvent<float> OnBottomPulling;
+    #endregion
+
     public void OnDrop(PointerEventData eventData)
     {
         throw new NotImplementedException();
