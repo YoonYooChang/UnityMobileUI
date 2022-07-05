55133c35e8b7a401f974d4edc6f29eca5bc43c1a
YoonYooChang <jell0202y@gmail.com>
2022-07-05 20:31:02 +0900

    Add PullScrollView Editor Script

---
+++ Assets/PullScrollView/Scripts/Editor/PullScrollViewEditor.cs
@@ -0,0 +1,16 @@
+using UnityEditor;
+using UnityEditor.UI;
+
+[CustomEditor(typeof(PullScrollView))]
+public class PullScrollViewEditor : ScrollRectEditor
+{
+    PullScrollView scrollView;
+
+    public override void OnInspectorGUI()
+    {
+        scrollView = (PullScrollView)target;
+
+        base.OnInspectorGUI();
+
+    }
+}
