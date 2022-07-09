45384d5682e7f1e698281579172c6eae3bb5c68d
YoonYooChang <jell0202y@gmail.com>
2022-07-09 23:01:18 +0900

    Add OnBeginDrag and OnEndDrag in PullScrollView

--- Assets/PullScrollView/Scripts/PullScrollView.cs
+++ Assets/PullScrollView/Scripts/PullScrollView.cs
@@ -1,23 +1,43 @@
 ﻿using System;
+using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class PullScrollView : ScrollRect, IDropHandler
+public class PullScrollView : ScrollRect
 {
     #region Event
     public UnityEvent OnTopPull;
     public UnityEvent OnBottomPull;
     public UnityEvent<float> OnTopPulling;
     public UnityEvent<float> OnBottomPulling;
-
-    private float previousPosition;
-
-    public float RequirePullDistance = 150f;
     #endregion
 
-    public void OnDrop(PointerEventData eventData)
+    private bool dragging;
+    private bool pullUp;
+    private bool pullDown;
+
+    private float previousPosition;
+    public float RequirePullDistance = 150f;
+
+    protected override void Start() => this.onValueChanged.AddListener(ScrollChanged);
+
+    private void ScrollChanged(Vector2 vector)
     {
-        throw new NotImplementedException();
+        
     }
+
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        base.OnBeginDrag(eventData);
+        dragging = true;
+    }
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        base.OnEndDrag(eventData);
+        dragging = false;
+    }
+
+
 }
