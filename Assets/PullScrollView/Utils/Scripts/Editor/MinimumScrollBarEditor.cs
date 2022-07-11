7f125b90a20305e4677aaad04be7a9dc968f0ef0
YoonYooChang <jell0202y@gmail.com>
2022-07-11 23:43:44 +0900

    Create MinimumScrollBar's editor script

---
+++ Assets/PullScrollView/Utils/Scripts/Editor/MinimumScrollBarEditor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MinimumScrollBar))]
+public class MinimumScrollBarEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+    }
+}
