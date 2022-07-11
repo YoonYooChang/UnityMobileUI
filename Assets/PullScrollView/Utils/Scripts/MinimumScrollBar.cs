7ac5228891407f4640f24a1b42d07e76efdbdf2f
YoonYooChang <jell0202y@gmail.com>
2022-07-11 23:43:26 +0900

    Update MinimumScrollBar.cs

--- Assets/PullScrollView/Utils/Scripts/MinimumScrollBar.cs
+++ Assets/PullScrollView/Utils/Scripts/MinimumScrollBar.cs
@@ -2,5 +2,13 @@ using UnityEngine.UI;
 
 public class MinimumScrollBar : Scrollbar
 {
-    
+    public float verticalScrollbarMinimumSize = 0.1f;
+
+    public override void Rebuild(CanvasUpdate executing)
+    {
+        base.Rebuild(executing);
+
+        if (size < verticalScrollbarMinimumSize)
+            size = verticalScrollbarMinimumSize;
+    }
 }
